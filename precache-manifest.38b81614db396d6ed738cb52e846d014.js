32417f7e493279ef809f9d56e428dfe5c8b187f1
Heron Hu <spectre.hhr@outlook.com>
2020-04-19 21:09:58 +0800

    make a react tic-tac-toe game

---
+++ precache-manifest.38b81614db396d6ed738cb52e846d014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1ac6e52e4caa1705ffead09d5a103a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed144fd1e5c85142a616",
+    "url": "/static/css/main.bf13b2b3.chunk.css"
+  },
+  {
+    "revision": "6c84d17184dc19a8d6af",
+    "url": "/static/js/2.fe3becc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe3becc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed144fd1e5c85142a616",
+    "url": "/static/js/main.40c4b425.chunk.js"
+  },
+  {
+    "revision": "271da6c74b3b85186610",
+    "url": "/static/js/runtime-main.43639289.js"
+  }
+]);
